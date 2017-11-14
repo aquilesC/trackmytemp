bf65aa4d79c6eae57793ee79f1db5b340412f0f2
Aquiles Carattino <aquiles@aquicarattino.com>
2017-11-14 17:13:50 +0100

    Sensors and Measurements are separated

--- flask_app.py
+++ flask_app.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from flask import Flask, redirect, render_template, request, url_for
 from flask_sqlalchemy import SQLAlchemy
 
@@ -6,9 +8,9 @@ app.config["DEBUG"] = True
 
 SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
     username="aquic",
-    password="mysql_password",
-    hostname="aquic.mysql.pythonanywhere-services.com",
-    databasename="aquic$default",
+    password="rsvp2370",
+    hostname="localhost",
+    databasename="sensors",
 )
 app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
 app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
@@ -16,20 +18,58 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
 
-class Comment(db.Model):
+class Sensor(db.Model):
 
-    __tablename__ = "comments"
+    __tablename__ = "sensors"
 
     id = db.Column(db.Integer, primary_key=True)
-    content = db.Column(db.String(4096))
+    name = db.Column(db.String(4096))
+    measurements = db.relationship('Measurement', backref='sensors', lazy=True)
+    creation = db.Column(db.Time())
+
+class Measurement(db.Model):
+
+    __tablename__ = "measurements"
+
+    id = db.Column(db.Integer, primary_key=True)
+    sensor_id = db.Column(db.Integer, db.ForeignKey('sensors.id'), nullable=False)
+    value = db.Column(db.Integer)
+    measure_time = db.Column(db.Time())
+    def __init__(self):
+        self.measure_time = datetime.now()
 
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "GET":
-        return render_template("main.html", comments=Comment.query.all())
+        return render_template("main.html", comments=Sensor.query.all())
 
-    comment = Comment(content=request.data)
-    db.session.add(comment)
+    sensor = Sensor(name=request.form['contents'])
+    sensor.creation = datetime.now()
+    db.session.add(sensor)
     db.session.commit()
 
     return redirect(url_for('index'))
+
+@app.route("/<int:sensor_id>", methods=["GET",])
+def show_sensor(sensor_id):
+    if request.method != "GET":
+        return "Wrong Method"
+
+    return render_template("sensor.html", sensor=Sensor.query.get(sensor_id))
+
+@app.route("/meas/<int:sensor_id>", methods=["GET", "POST"])
+def measurement(sensor_id):
+    if request.method == "GET":
+        s = Sensor.query.get(sensor_id)
+        m = Measurement.query.filter_by(sensor_id=sensor_id)
+        return render_template("measurement.html", sensor=s, measurements=m)
+
+    elif request.method == "POST":
+        value = request.data
+        m = Measurement()
+
+
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', 8080, True)
